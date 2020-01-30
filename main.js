7ce3ea2d17e02b0ee35d9b5bd6f8f6354a5d3489
Brian Bonsignore <bonsignore.brian@gmail.com>
2020-01-29 23:13:09 -0500

    Adds simple event listeners

--- main.js
+++ main.js
@@ -1,16 +1,43 @@
 
-// create main html elements
-
-let buttons = document.querySelectorAll(".button");
-
+// ============= Add event listeners =============
+let inputButtons = document.querySelectorAll(".button.print");
 for (let button of buttons) {
-    button.addEventListener("click", buttonListerner)
+    button.addEventListener("click", buttonListerner) //defined
 }
 
-let disp = document.querySelector(".display");
+let equalButton = document.querySelector(".eval");
+equals.addEventListener("click", evaluateInput); // **** TODO
 
-function buttonListerner (event){
+let clearButton = document.querySelector(".clear");
+clearButton.addEventListener("click", clearInput) // defined
+
+
+// Set up variables to control the display
+let display = document.querySelector(".display");
+let displayHistory = [];
+let inputArea = document.querySelector(".input");
+let inputString = inputArea.innerText;
+
+function trimHistory() {
+    if (displayLines.length > 3) {
+        displayLines.shift();
+    }
+}
+
+function pushToHistory(string) {
+    displayLines.push(string);
+    trimHistory();
+}
+
+// ===== Callback functions for eventlisteners ======
+function buttonListerner(event) {
     let whichButton = event.target;
-    disp.innerText += whichButton.innerText
+    inputString += whichButton.innerText
     // console.log(whichButton.innerText);
 }
+
+function clearInput(event) {
+    inputString = ""
+}
+
+function
