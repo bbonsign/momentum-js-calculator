a2a30ff8ae58a23404e0ea1c95e76a04325cafb3
Brian Bonsignore <bonsignore.brian@gmail.com>
2020-01-29 23:45:30 -0500

    Adds math.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 
 // ============= Add event listeners =============
 let inputButtons = document.querySelectorAll(".button.print");
-for (let button of buttons) {
+for (let button of inputButtons) {
     button.addEventListener("click", buttonListerner) //defined
 }
 
 let equalButton = document.querySelector(".eval");
-equals.addEventListener("click", evaluateInput); // **** TODO
+equalButton.addEventListener("click", evaluateInput); // **** TODO
 
 let clearButton = document.querySelector(".clear");
 clearButton.addEventListener("click", clearInput) // defined
@@ -15,8 +15,9 @@ clearButton.addEventListener("click", clearInput) // defined
 // Set up variables to control the display
 let display = document.querySelector(".display");
 let displayHistory = [];
+
 let inputArea = document.querySelector(".input");
-let inputString = inputArea.innerText;
+let inputString = "";
 
 function trimHistory() {
     if (displayLines.length > 3) {
@@ -32,12 +33,16 @@ function pushToHistory(string) {
 // ===== Callback functions for eventlisteners ======
 function buttonListerner(event) {
     let whichButton = event.target;
-    inputString += whichButton.innerText
+    inputString += whichButton.dataset.txt;
+    inputArea.innerText = inputString;
     // console.log(whichButton.innerText);
 }
 
-function clearInput(event) {
-    inputString = ""
+function clearInput() {
+    inputString = "";
+    inputArea.innerText = inputString;
 }
 
-function
+function evaluateInput(event) {
+    console.log(math.evaluate(inputString));
+}
