2c958bafb4b6c121345bd6eb7a87765185a57952
Brian Bonsignore <bonsignore.brian@gmail.com>
2020-01-30 01:17:02 -0500

    Finishes event listeners and display functions

--- main.js
+++ main.js
@@ -6,43 +6,107 @@ for (let button of inputButtons) {
 }
 
 let equalButton = document.querySelector(".eval");
-equalButton.addEventListener("click", evaluateInput); // **** TODO
+equalButton.addEventListener("click", evaluateInput); // **** TODO: partially done
 
 let clearButton = document.querySelector(".clear");
 clearButton.addEventListener("click", clearInput) // defined
 
+let answerButton = document.querySelector(".ans");
+answerButton.addEventListener("click", appendPreviousAnswer)
+
 
 // Set up variables to control the display
 let display = document.querySelector(".display");
-let displayHistory = [];
+let history = []; // will be an array or array pairs: [ [ "input sting", "result"], ...]
 
 let inputArea = document.querySelector(".input");
 let inputString = "";
 
+function updateInputArea(){
+    inputArea.innerText = inputString;
+}
+
 function trimHistory() {
-    if (displayLines.length > 3) {
-        displayLines.shift();
+    if (history.length > 3) {
+        history.pop();
     }
 }
 
-function pushToHistory(string) {
-    displayLines.push(string);
-    trimHistory();
+function addToHistory(input, result) {
+    if (result != undefined) {
+        history.unshift([input, result]);
+        trimHistory();
+    }
+}
+
+function clearDisplayHistory() {
+    let displayHistoryElements = document.querySelectorAll(".history");
+    // if (displayHistoryElements.length > 0) {
+    for (let i = 0; i < displayHistoryElements.length ; i++) {
+        displayHistoryElements[i].remove();
+    }
+    // }
+}
+
+function createHistoryDisplay() {
+    clearDisplayHistory();
+    // display.innerHTML = `<p class="input"></p>`;
+
+    let c = 1;
+    let historyReversed = history.slice().reverse();
+    for (let pair of historyReversed) {
+        let oldInput = pair[0];
+        let oldAnswer = pair[1];
+
+        let pInput = document.createElement("p");
+        let pAnswer = document.createElement("p");
+        pInput.innerText = oldInput;
+        pAnswer.innerText = oldAnswer + " ⇐"
+
+        pInput.classList.add("history");
+        pAnswer.classList.add("history");
+
+        if (c % 2 == 0) {
+            pInput.classList.add("even");
+            pAnswer.classList.add("even");
+        }
+        else {
+            pInput.classList.add("odd");
+            pAnswer.classList.add("odd");
+        }
+
+        display.insertBefore(pInput, inputArea);
+        display.insertBefore(pAnswer, inputArea);
+
+        c++
+    }
 }
 
 // ===== Callback functions for eventlisteners ======
 function buttonListerner(event) {
     let whichButton = event.target;
     inputString += whichButton.dataset.txt;
-    inputArea.innerText = inputString;
-    // console.log(whichButton.innerText);
+    updateInputArea();
 }
 
 function clearInput() {
     inputString = "";
-    inputArea.innerText = inputString;
+    updateInputArea();
 }
 
 function evaluateInput(event) {
-    console.log(math.evaluate(inputString));
+    let answer = math.evaluate(inputString);
+    addToHistory(inputString, answer);
+
+    createHistoryDisplay()
+    clearInput();
+}
+
+// TODO
+function appendPreviousAnswer() {
+    if (history.length == 0) {
+        inputString += "";
+    }
+    inputString += history[0][1];
+    updateInputArea();
 }
